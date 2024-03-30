89b5bf21e0ddd7466ed57b92fc19bf6d1343e3c3
robertjruiz <robertjruiz03@gmail.com>
2024-03-29 21:08:37 -0700

    implemented multiple ticker price input capability

--- tickchart.py
+++ tickchart.py
@@ -8,21 +8,21 @@ import numpy as np
 import matplotlib.pyplot as plt  
 
 
+#displays the title of the program
 f = open('title.txt', 'r')
-
-
 file_contents = f.read()
-
 print(file_contents)
-
 f.close()
 
 
+input_string = input('Enter tickers sperated by a space: \n')
+tickers = input_string.split()
 
-ticker = input("type ticker here: ")
+#ticker = input("type ticker here: ")
 
-stocks = [ticker]
+stocks = [tickers]
 
+'''
 sd_date_entry = input('Enter a start date in YYYY-MM-DD format: ')
 sd_year, sd_month, sd_day = map(int, sd_date_entry.split('-'))
 sd_date1 = dt.date(sd_year, sd_month, sd_day)
@@ -30,22 +30,30 @@ sd_date1 = dt.date(sd_year, sd_month, sd_day)
 ed_date_entry = input('Enter a end date in YYYY-MM-DD format: ')
 ed_year, ed_month, ed_day = map(int, ed_date_entry.split('-'))
 ed_date1 = dt.date(ed_year, ed_month, ed_day)
+'''
+#temporary date range for quicker testing
+sd_date1 = dt.date(2020, 1, 1)
+ed_date1 = dt.date(2023, 12, 31)
 
 
-
-stocktick = yf.Ticker(ticker)
+'''
+for ticker in tickers:
+    stocktick = yf.Ticker(stocks)
 companyname = stocktick.info['longName']
 
 print(companyname + " data loading...")
+'''
 
 
+df = pd.DataFrame()
+for ticker in tickers:
+    data = yf.download(ticker, start=sd_date1, end=ed_date1)
+    df[ticker] = data['Close']
 
-df = yf.download(stocks, start = sd_date1, end = ed_date1)
 
+df.plot(figsize=(10, 6))
+plt.title(input_string + ' Prices')
 
-close_price = df['Close']
-
-close_price.plot(title = companyname + " Price", figsize = (10,6))
-
+plt.legend()
 plt.show()
 
