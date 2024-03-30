5849075451b5043ad256b740817bf273ab033133
robertjruiz <robertjruiz03@gmail.com>
2024-03-29 21:50:09 -0700

    cumulative return of market

--- tickchart.py
+++ tickchart.py
@@ -1,5 +1,4 @@
 
-
 import yfinance as yf
 import pandas as pd 
 from pandas_datareader import data as pdr 
@@ -18,10 +17,9 @@ f.close()
 input_string = input('Enter tickers sperated by a space: \n')
 tickers = input_string.split()
 
-#ticker = input("type ticker here: ")
+#'''
 
-stocks = [tickers]
-
+#user input for date range
 sd_date_entry = input('Enter a start date in YYYY-MM-DD format: ')
 sd_year, sd_month, sd_day = map(int, sd_date_entry.split('-'))
 sd_date1 = dt.date(sd_year, sd_month, sd_day)
@@ -41,12 +39,16 @@ ed_date1 = dt.date(2023, 12, 31)
 df = pd.DataFrame()
 for ticker in tickers:
     data = yf.download(ticker, start=sd_date1, end=ed_date1)
-    df[ticker] = data['Close']
+    df[ticker] = data['Adj Close']
 
+returns = df.apply(lambda x: (x / x.iloc[0] - 1) * 100)
 
-df.plot(figsize=(10, 6))
+returns.plot(figsize=(10, 6))
 plt.title(input_string + ' Prices')
 
 plt.legend()
+plt.ylabel('Cumulative Return %')
+plt.xlabel('Time')
+plt.grid(lw=0.2)
 plt.show()
 
